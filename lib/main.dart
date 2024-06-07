7fff856fa0e1c06ab62a7e65336eb18706fef3ce
uratarin <sabrinagauel@gmail.com>
2024-06-07 18:40:08 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'todo_add_page.dart';
+import 'todo_item.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDoリスト',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: ToDoListPage(),
+    );
+  }
+}
+
+class ToDoListPage extends StatefulWidget {
+  @override
+  _ToDoListPageState createState() => _ToDoListPageState();
+}
+
+class _ToDoListPageState extends State<ToDoListPage> {
+  List<String> toDoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoItem(
+              item: toDoList[index],
+              onDelete: () {
+                setState(() {
+                  toDoList.removeAt(index);
+                });
+              });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return ToDoAddPage();
+            }),
+          );
+          if (newListText != null) {
+            setState(() {
+              toDoList.add(newListText);
+            });
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
